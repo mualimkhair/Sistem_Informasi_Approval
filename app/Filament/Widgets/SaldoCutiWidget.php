0244aab461dfc6f3532519b7d4f4b3409fdade59
AdityaAksar <54211361+AdityaAksar@users.noreply.github.com>
2026-07-01 14:28:54 +0800

    style(ui): update cuti widget

--- app/Filament/Widgets/SaldoCutiWidget.php
+++ app/Filament/Widgets/SaldoCutiWidget.php
@@ -17,10 +17,16 @@ class SaldoCutiWidget extends BaseWidget
             Stat::make('Sisa Cuti Tahunan', $saldo->saldo_n + $saldo->saldo_n1 + $saldo->saldo_n2 . ' Hari')
                 ->description("N: {$saldo->saldo_n} | N-1: {$saldo->saldo_n1} | N-2: {$saldo->saldo_n2}")
                 ->icon('heroicon-o-calendar-days'),
-            Stat::make('Cuti Besar / Sakit', $saldo->saldo_cuti_besar . ' / ' . $saldo->saldo_cuti_sakit . ' Hari')
-                ->icon('heroicon-o-briefcase'),
-            Stat::make('Melahirkan / Alasan Penting', $saldo->saldo_cuti_melahirkan . ' / ' . $saldo->saldo_cuti_alasan_penting . ' Hari')
+            Stat::make('Cuti Besar', $saldo->saldo_cuti_besar . ' / ' . '90' . ' Hari')
+                ->icon('heroicon-o-calendar-days'),
+            Stat::make('Cuti Sakit', $saldo->saldo_cuti_sakit . ' / ' . '365' . ' Hari')
+                ->icon('heroicon-o-heart'),
+            Stat::make('Cuti Melahirkan', $saldo->saldo_cuti_melahirkan . ' / ' . '90' . ' Hari')
+                ->icon('heroicon-m-user'),
+            Stat::make('Cuti Alasan Penting', $saldo->saldo_cuti_alasan_penting . ' / ' . '30' . ' Hari')
                 ->icon('heroicon-o-exclamation-circle'),
+            // Stat::make('Cuti Diluar Tanggungan Negara', $saldo->saldo_cuti_diluar_tanggungan_negara . ' / ' . '1095' . ' Hari')
+            //     ->icon('heroicon-o-exclamation-circle'),
         ];
     }
 }
